9cccef6a254bd4b479e9e71b5bbdf249e9186565
Spherz <spherzpro228@gmail.com>
2024-04-02 16:33:11 +0200

    update vite config

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: "https://github.com/Spherz/nike.react.learning.github.io.git",
   plugins: [react()],
+  server: {
+    host: true,
+  },
 })
